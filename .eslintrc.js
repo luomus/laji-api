3bd319785cdf5be3407599dd68ccc84a4149666f
Olli Raitio <olli.raitio@helsinki.fi>
2023-02-23 12:38:55 +0200

    rm prettier, use tabs instead of spaces

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,8 +7,7 @@ module.exports = {
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended'
   ],
   root: true,
   env: {
@@ -21,5 +20,9 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+		"indent": [
+			"error",
+			"tab"
+		]
   },
 };
