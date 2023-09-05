f747570882e1f86556883b0931146bf9e2ef7f80
Olli Raitio <olli.raitio@helsinki.fi>
2023-09-05 15:58:05 +0300

    try to fix redirect to old api query params not passed for post

--- src/proxy-to-old-api/proxy-to-old-api.service.ts
+++ src/proxy-to-old-api/proxy-to-old-api.service.ts
@@ -8,15 +8,16 @@ const OLD_API = "http://localhost:3003/v0";
 export class ProxyToOldApiService {
 	async redirectToOldApi(request: Request, response: Response) {
 		// Taken from https://stackoverflow.com/a/62289124
-		const method = request.method.toLowerCase();
 		const oldApiRequestUrl = OLD_API + request.path;
+		console.log(request.query);
 		const redirectedRequest = _request({
 			uri: oldApiRequestUrl,
+			method: request.method,
+			body: request.readable ? undefined : request.body,
+			json: request.readable ? false : true,
 			qs: request.query,
-			headers: request.header,
-			method: method.toUpperCase(),
-			json: request.readable ? false: true,
-			body: request.readable ? undefined : request.body
+			// Pass redirect back to the browser
+			followRedirect: false
 		});
 		if (request.readable) {
 			// Handles all the streamable data (e.g. image uploads)
