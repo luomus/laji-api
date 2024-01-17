2d48ee6589b2df9aeb1d0976711365b93d75bfcc
aorin <meeri.rannisto@gmail.com>
2024-01-17 15:05:44 +0200

    fix idIn parameter missing from swagger

--- src/audio/audio.controller.ts
+++ src/audio/audio.controller.ts
@@ -1,15 +1,15 @@
 import {
-	Body,
-	Controller,
-	Delete,
-	Get, HttpCode, HttpStatus,
-	Param,
-	Post,
-	Put,
-	Query,
-	Req,
-	Res,
-	UseInterceptors
+    Body,
+    Controller,
+    Delete,
+    Get, HttpCode, HttpStatus,
+    Param,
+    Post,
+    Put,
+    Query,
+    Req,
+    Res,
+    UseInterceptors
 } from "@nestjs/common";
 import { ApiOkResponse, ApiSecurity, ApiTags } from "@nestjs/swagger";
 import { AbstractMediaService } from "../abstract-media/abstract-media.service";
@@ -23,32 +23,32 @@ import { FindOneDto, GetPageDto, QueryWithPersonTokenDto } from "../common.dto";
 @Controller("audio")
 @ApiTags("Audio")
 export class AudioController {
-	constructor(
+    constructor(
         private abstractMediaService: AbstractMediaService,
-	) {}
+    ) {}
 
     /** Get all audio */
     @Get()
     @UseInterceptors(createQueryParamsInterceptor(GetPageDto, Audio))
-	async getAll(@Query() { idIn }: GetPageDto): Promise<Audio[]> {
-		return this.abstractMediaService.getMedia(MediaType.audio, idIn);
-	}
+    async getAll(@Query() { idIn }: GetPageDto): Promise<Audio[]> {
+        return this.abstractMediaService.getMedia(MediaType.audio, idIn);
+    }
 
     /** Upload audio and get temporary id */
     @Post()
     @ApiOkResponse({
-    	type: FileUploadResponse
+        type: FileUploadResponse
     })
     async upload(@Query() { personToken }: QueryWithPersonTokenDto, @Req() req: Request, @Res() res: Response) {
-    	const proxy = await this.abstractMediaService.getUploadProxy(MediaType.audio, personToken);
-    	req.pipe(proxy).pipe(res);
+        const proxy = await this.abstractMediaService.getUploadProxy(MediaType.audio, personToken);
+        req.pipe(proxy).pipe(res);
     }
 
     /** Get audio by id */
     @Get(":id")
     @UseInterceptors(createQueryParamsInterceptor(FindOneDto, Audio))
     findOne(@Param("id") id: string, @Query() {}: FindOneDto): Promise<Audio> {
-    	return this.abstractMediaService.findOne(MediaType.audio, id);
+        return this.abstractMediaService.findOne(MediaType.audio, id);
     }
 
     /** Update audio metadata */
@@ -64,31 +64,31 @@ export class AudioController {
     @Delete(":id")
     @HttpCode(HttpStatus.NO_CONTENT)
     delete(@Param("id") id: string, @Query() { personToken }: QueryWithPersonTokenDto) {
-    	return this.abstractMediaService.deleteMedia(MediaType.audio, id, personToken);
+        return this.abstractMediaService.deleteMedia(MediaType.audio, id, personToken);
     }
 
     /** Fetch mp3 by id */
     @Get(":id/mp3")
     findMp3(@Param("id") id: string, @Res() res: Response) {
-    	this.abstractMediaService.findURL(MediaType.audio, id, "mp3URL").then(url => {
-    		res.redirect(url);
-    	});
+        this.abstractMediaService.findURL(MediaType.audio, id, "mp3URL").then(url => {
+            res.redirect(url);
+        });
     }
 
     /** Fetch thumbnail by id */
     @Get(":id/thumbnail.jpg")
     findThumbnail(@Param("id") id: string, @Res() res: Response) {
-    	this.abstractMediaService.findURL(MediaType.audio, id, "thumbnailURL").then(url => {
-    		res.redirect(url);
-    	});
+        this.abstractMediaService.findURL(MediaType.audio, id, "thumbnailURL").then(url => {
+            res.redirect(url);
+        });
     }
 
     /** Fetch wav by id */
     @Get(":id/wav")
     findWav(@Param("id") id: string, @Res() res: Response) {
-    	this.abstractMediaService.findURL(MediaType.audio, id, "wavURL").then(url => {
-    		res.redirect(url);
-    	});
+        this.abstractMediaService.findURL(MediaType.audio, id, "wavURL").then(url => {
+            res.redirect(url);
+        });
     }
 
     /** Upload audio metadata */
@@ -97,6 +97,6 @@ export class AudioController {
     async uploadMetadata(
         @Param("tempId") tempId: string, @Query() { personToken }: QueryWithPersonTokenDto, @Body() audio: Audio
     ): Promise<Audio> {
-    	return this.abstractMediaService.uploadMetadata(MediaType.audio, tempId, audio, personToken);
+        return this.abstractMediaService.uploadMetadata(MediaType.audio, tempId, audio, personToken);
     }
 }
--- src/images/images.controller.ts
+++ src/images/images.controller.ts
@@ -1,15 +1,15 @@
 import {
-	Body,
-	Controller,
-	Delete,
-	Get, HttpCode, HttpStatus,
-	Param,
-	Post,
-	Put,
-	Query,
-	Req,
-	Res,
-	UseInterceptors
+    Body,
+    Controller,
+    Delete,
+    Get, HttpCode, HttpStatus,
+    Param,
+    Post,
+    Put,
+    Query,
+    Req,
+    Res,
+    UseInterceptors
 } from "@nestjs/common";
 import { ApiOkResponse, ApiSecurity, ApiTags } from "@nestjs/swagger";
 import { Request, Response } from "express";
@@ -23,30 +23,30 @@ import { FindOneDto, GetPageDto, QueryWithPersonTokenDto } from "../common.dto";
 @Controller("images")
 @ApiTags("Image")
 export class ImagesController {
-	constructor(
+    constructor(
         private abstractMediaService: AbstractMediaService
-	) {}
+    ) {}
 
     /** Get all images */
     @Get()
     @UseInterceptors(createQueryParamsInterceptor(GetPageDto, Image))
-	async getAll(@Query() { idIn }: GetPageDto): Promise<Image[]> {
-		return this.abstractMediaService.getMedia(MediaType.image, idIn);
-	}
+    async getAll(@Query() { idIn }: GetPageDto): Promise<Image[]> {
+        return this.abstractMediaService.getMedia(MediaType.image, idIn);
+    }
 
     /** Upload image and get temporary id */
     @Post()
     @ApiOkResponse({ type: FileUploadResponse })
     async upload(@Query() { personToken }: QueryWithPersonTokenDto, @Req() req: Request, @Res() res: Response) {
-    	const proxy = await this.abstractMediaService.getUploadProxy(MediaType.image, personToken);
-    	req.pipe(proxy).pipe(res);
+        const proxy = await this.abstractMediaService.getUploadProxy(MediaType.image, personToken);
+        req.pipe(proxy).pipe(res);
     }
 
     /** Get image by id */
     @Get(":id")
     @UseInterceptors(createQueryParamsInterceptor(FindOneDto, Image))
     findOne(@Param("id") id: string, @Query() {}: FindOneDto): Promise<Image> {
-    	return this.abstractMediaService.findOne(MediaType.image, id);
+        return this.abstractMediaService.findOne(MediaType.image, id);
     }
 
     /** Update image metadata */
@@ -55,38 +55,38 @@ export class ImagesController {
     updateMetadata(
         @Param("id") id: string, @Query() { personToken }: QueryWithPersonTokenDto, @Body() image: Image
     ): Promise<Image> {
-    	return this.abstractMediaService.updateMetadata(MediaType.image, id, image, personToken);
+        return this.abstractMediaService.updateMetadata(MediaType.image, id, image, personToken);
     }
 
     /** Delete image */
     @Delete(":id")
     @HttpCode(HttpStatus.NO_CONTENT)
     delete(@Param("id") id: string, @Query() { personToken }: QueryWithPersonTokenDto) {
-    	return this.abstractMediaService.deleteMedia(MediaType.image, id, personToken);
+        return this.abstractMediaService.deleteMedia(MediaType.image, id, personToken);
     }
 
     /** Fetch large image by id */
     @Get(":id/large.jpg")
     findLarge(@Param("id") id: string, @Res() res: Response) {
-    	this.abstractMediaService.findURL(MediaType.image, id, "largeURL").then(url => {
-    		res.redirect(url);
-    	});
+        this.abstractMediaService.findURL(MediaType.image, id, "largeURL").then(url => {
+            res.redirect(url);
+        });
     }
 
     /** Fetch square thumbnail by id */
     @Get(":id/square.jpg")
     findSquare(@Param("id") id: string, @Res() res: Response) {
-    	this.abstractMediaService.findURL(MediaType.image, id, "squareThumbnailURL").then(url => {
-    		res.redirect(url);
-    	});
+        this.abstractMediaService.findURL(MediaType.image, id, "squareThumbnailURL").then(url => {
+            res.redirect(url);
+        });
     }
 
     /** Fetch thumbnail by id */
     @Get(":id/thumbnail.jpg")
     findThumbnail(@Param("id") id: string, @Res() res: Response) {
-    	this.abstractMediaService.findURL(MediaType.image, id, "thumbnailURL").then(url => {
-    		res.redirect(url);
-    	});
+        this.abstractMediaService.findURL(MediaType.image, id, "thumbnailURL").then(url => {
+            res.redirect(url);
+        });
     }
 
     /** Upload image metadata */
@@ -95,7 +95,7 @@ export class ImagesController {
     async uploadMetadata(
         @Param("tempId") tempId: string, @Query() { personToken }: QueryWithPersonTokenDto, @Body() image: Image
     ): Promise<Image> {
-    	return this.abstractMediaService.uploadMetadata(MediaType.image, tempId, image, personToken);
+        return this.abstractMediaService.uploadMetadata(MediaType.image, tempId, image, personToken);
     }
 }
 
