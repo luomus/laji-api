9afdc2a322e901091aa8f16252ec0862c7a06d65
Olli Raitio <olli.raitio@helsinki.fi>
2024-01-22 13:32:36 +0200

    forgot to push serialize decorator & swagger scanner

---
+++ src/serializing/serialize.decorator.ts
@@ -0,0 +1,66 @@
+import { Newable, isObject } from "src/type-utils";
+import { SerializeOptions } from "src/serializing/serializing";
+import { createSwaggerScanner } from "src/swagger/swagger-scanner";
+import { UseInterceptors, applyDecorators } from "@nestjs/common";
+import { createNewSerializingInterceptorWith } from "./serializing.interceptor";
+
+const SERIALIZE_METADATA = "SERIALIZE_METADATA";
+
+type HasSchemaDefinitionName = { schemaDefinitionName: string };
+
+type SerializeBase = {
+	serializeInto: Newable<unknown>;
+};
+type SerializeEntryWithWhiteList = SerializeBase
+	& { serializeOptions: HasWhitelist }
+	& HasSchemaDefinitionName;
+type SerializeEntryWithoutWhiteList = SerializeBase
+	& { serializeOptions?: HasNotWhitelist }
+	& Partial<HasSchemaDefinitionName>;
+export type SerializeEntry = SerializeEntryWithWhiteList | SerializeEntryWithoutWhiteList;
+
+export const entryHasWhiteList = (entry: SerializeEntry): entry is SerializeEntryWithWhiteList => {
+	return (entry.serializeOptions as any)?.whitelist;
+}
+
+export const isSerializeEntry = (entry: unknown): entry is SerializeEntry =>
+	isObject(entry) && "serializeInto" in entry;
+
+type HasWhitelist = SerializeOptions & Required<Pick<SerializeOptions, "whitelist">>;
+type HasNotWhitelist = Omit<SerializeOptions, "whitelist">
+
+type SReturnT = (target: any, propertyKey: any) => void;
+
+type SerializeOverload = { 
+	(serializeInto: Newable<unknown>, serializeOptions: HasWhitelist, schemaDefinitionName: string): SReturnT
+	(serializeInto: Newable<unknown>, serializeOptions?: HasNotWhitelist, schemaDefinitionName?: string): SReturnT;
+}
+
+export const SERIALIZE_OPTIONS_METADATA = "SERIALIZE_OPTIONS_METADATA";
+
+/**
+ * Apply our custom serialization to the response. If you provide a whitelist, it's mandatory to also provide a name
+ * for the Swagger schema definition.
+ */
+export const Serialize: SerializeOverload = (...params) =>
+	applyDecorators(
+		SwaggerSerializeOptionsApplied(...params),
+		UseInterceptors(createNewSerializingInterceptorWith(params[0], params[1]))
+	);
+
+export const SwaggerSerializeOptionsApplied: SerializeOverload = (
+	serializeInto: Newable<unknown>,
+	serializeOptions?: SerializeOptions,
+	schemaDefinitionName?: string
+) => {
+	return function (target: any, propertyKey: any) {
+		const entry: SerializeEntry = { serializeInto, serializeOptions, schemaDefinitionName };
+		Reflect.defineMetadata(
+			SERIALIZE_METADATA + propertyKey,
+			entry,
+			target
+		);
+	}
+};
+
+export const SerializeScanner = createSwaggerScanner(SERIALIZE_METADATA);
---
+++ src/swagger/swagger-scanner.ts
@@ -0,0 +1,44 @@
+import { RequestMethod } from "@nestjs/common";
+import { METHOD_METADATA, PATH_METADATA } from "@nestjs/common/constants";
+import { SwaggerRemoteRefEntry } from "./swagger-remote.decorator";
+import { SerializeEntry } from "src/serializing/serialize.decorator";
+
+export type SwaggerCustomizationEntry = SwaggerRemoteRefEntry | SerializeEntry;
+
+export const swaggerCustomizationEntries: {
+	[path: string]: {
+		[method: string]: {
+			[responseCode: string]: SwaggerCustomizationEntry[]
+		}
+	}
+} = {};
+
+
+/**
+ * Creates a class controller that allow usage of a method decorator controlled with param `metadataKey`.
+ *
+ * The created class decorator must be applied before the `@Controller()` decorator!
+ * */
+export function createSwaggerScanner(metadataKey: string) {
+	return () => (target: any) => {
+		Reflect.ownKeys(target.prototype).forEach(propertyKey => {
+			const entry = Reflect.getMetadata(metadataKey + (propertyKey as string), target.prototype);
+
+			if (!entry) {
+				return;
+			}
+			// The path defined by `@Controller()` decorator.
+			const path = Reflect.getMetadata(PATH_METADATA, target);
+
+			// The method defined by methods method decorator (`@Get()`, `@Post()`, ...).
+			const method = RequestMethod[Reflect.getMetadata(METHOD_METADATA, target.prototype[propertyKey])];
+			const responseCode = method === "POST"
+				? 201 : 200;
+			const existingEntries = swaggerCustomizationEntries[path]?.[(propertyKey as string)]?.[responseCode] || [];
+			swaggerCustomizationEntries[path] = {
+				...(swaggerCustomizationEntries[path] || {}),
+				[propertyKey]: { [responseCode]: [ ...existingEntries, entry ] }
+			}
+		});
+	}
+}
