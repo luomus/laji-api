f0561058b5efffb41ccf0061ae8d8da3d7576cdf
Olli Raitio <olli.raitio@helsinki.fi>
2024-01-24 15:17:19 +0200

    rm unused @ApiSecurity from warehouse controller as it's handled by swagger service

--- src/warehouse/warehouse.controller.ts
+++ src/warehouse/warehouse.controller.ts
@@ -2,9 +2,8 @@ import { createProxyMiddleware } from "http-proxy-middleware";
 import { All, Controller, Next, Req, Res } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { NextFunction, Request, Response } from "express";
-import { ApiExcludeController, ApiSecurity } from "@nestjs/swagger";
+import { ApiExcludeController } from "@nestjs/swagger";
 
-@ApiSecurity("access_token")
 @Controller("warehouse")
 @ApiExcludeController() // Warehouse has it's own swagger doc, which is merged to ours by SwaggerService.
 export class WarehouseController {
