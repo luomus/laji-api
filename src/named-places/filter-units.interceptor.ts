eba24cdf27c6ec3512dee591daa0f1d7785a908b
Olli Raitio <olli.raitio@helsinki.fi>
2024-06-05 14:27:17 +0300

    fix lint issues

--- src/named-places/filter-units.interceptor.ts
+++ src/named-places/filter-units.interceptor.ts
@@ -29,7 +29,7 @@ export class FilterUnitsInterceptor implements NestInterceptor {
 			return shouldIncludeUnits
 				? place
 				: serializeInto(NamedPlaceUnitsFiltered)(place);
-		}
+		};
 	}
 }
 
