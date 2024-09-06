a5c6e99e27b69a856aed001a8cfa81cb796f954c
Olli Raitio <olli.raitio@helsinki.fi>
2024-09-06 11:23:28 +0300

    add missing checklist dto file

---
+++ src/checklist/checklist.dto.ts
@@ -0,0 +1,13 @@
+import { IntersectionType } from "@nestjs/swagger";
+import { PagedDto, LangQueryDto } from "../common.dto";
+import { CommaSeparatedStrings } from "src/serializing/serializing";
+import { Checklist as _Checklist } from "@luomus/laji-schema";
+
+export class GetChecklistPageDto extends IntersectionType(LangQueryDto, PagedDto) {
+	/**	Include only items with the given ids. Multiple values are separated by a comma (,). */
+	@CommaSeparatedStrings() idIn?: string[];
+}
+
+export class GetChecklistDto extends IntersectionType(LangQueryDto) {}
+
+export type Checklist = _Checklist & { id: string };
