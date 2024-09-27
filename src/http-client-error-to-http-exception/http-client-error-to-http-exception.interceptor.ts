c3e8b553383239f7aac8d65e332851c1f3a42416
Olli Raitio <olli.raitio@helsinki.fi>
2024-09-27 17:41:36 +0300

    http-client-error-to-http-exception-interceptor logs incoming request

--- src/http-client-error-to-http-exception/http-client-error-to-http-exception.interceptor.ts
+++ src/http-client-error-to-http-exception/http-client-error-to-http-exception.interceptor.ts
@@ -1,5 +1,6 @@
 import { CallHandler, ExecutionContext, HttpException, Injectable, Logger, NestInterceptor } from "@nestjs/common";
 import { catchError, Observable, throwError } from "rxjs";
+import { Request } from "express";
 
 /**
  * Catches AxiosErrors where the request fails due to a error response, and rethrow it as an HttpException.
@@ -10,8 +11,14 @@ export class HttpClientErrorToHttpExceptionInterceptor implements NestIntercepto
 	logger = new Logger("HttpClientError");
 
 	intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+		const request = context.switchToHttp().getRequest<Request>();
 		return next.handle().pipe(catchError(e => {
-			this.logger.error(e, { status: e.response?.status, body: e.response?.body });
+			this.logger.error(e, {
+				incomingRequest: request.url,
+				incomingRequestBody: request.body,
+				outgoingResponseStatus: e.response?.status,
+				outgoingResponseBody: e.response?.body
+			});
 			return throwError(() =>  {
 				return (!(e instanceof HttpException) && e.response?.status)
 					? new HttpException(e.response?.data as any ?? "LajiApi generic error - External request failed",
