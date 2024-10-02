9e88168ac7c2b6e6c1a413ac472bb5227cb0c170
Olli Raitio <olli.raitio@helsinki.fi>
2024-10-02 15:39:03 +0300

    fix lint issues

--- src/request.ts
+++ src/request.ts
@@ -1,6 +1,5 @@
 import { Request as _Request } from "express";
 import { Person } from "./persons/person.dto";
-import { ApiUsersService } from "./api-users/api-users.service";
 import { ApiUserEntity } from "./api-users/api-user.entity";
 
 export type Request = _Request & {
