luomus/laji-api

commit 289696a9ad75269d2a0fbe90f58894ae14c6e047

fix form permission check [closes luomus/laji#600]

src/forms/form-permissions/form-permissions.service.ts
@@ -50,7 +50,7 @@ export class FormPermissionsService {
 	async getByCollectionIDAndPerson(collectionID: string, person?: Person): Promise<FormPermissionDto> {
 		const permissions = await this.findByCollectionIDAndPerson(collectionID, person);
 		if (!permissions) {
-			throw new HttpException("Form does not have restrict feature enabled", 404);
+			throw new HttpException("Form does not have MHL.restrictAccess or MHL.hasAdmins enabled", 404);
 		}
 		return permissions;
 	}
@@ -100,7 +100,7 @@ export class FormPermissionsService {
 	async requestAccess(collectionID: string, person: Person) {
 		const permissions = await this.getByCollectionIDAndPerson(collectionID, person);
 
-		if (hasEditRightsOf(permissions, person)) {
+		if (permissions.editors.includes(person.id) || permissions.admins.includes(person.id)) {
 			throw new HttpException("You already have access to this form", 406);
 		}
 
@@ -200,11 +200,7 @@ export class FormPermissionsService {
 
 	async hasEditRightsOf(collectionID: string, person: Person) {
 		const permissions = await this.findByCollectionIDAndPerson(collectionID, person);
-		const form = await this.findFormWithPermissionFeature(collectionID);
-		if (!form?.options.restrictAccess) {
-			return true;
-		}
-		return hasEditRightsOf(permissions, person);
+		return !permissions ||  hasEditRightsOf(permissions, person);
 	}
 
 	private async sendFormPermissionRequested(person: Person, collectionID: string) {
@@ -269,10 +265,8 @@ const isAdminOf = (permissions: PermissionLists, person: Person) =>
 	person.role?.some(r => r === Role.Admin)
 		|| permissions.admins?.includes(person.id);
 
-const hasEditRightsOf = (permissions: FormPermissionDto | undefined, person: Person) =>
-	!permissions
-		|| isAdminOf(permissions, person)
-		|| permissions.editors.includes(person.id);
+const hasEditRightsOf = (permissions: FormPermissionDto, person: Person) =>
+	isAdminOf(permissions, person) || permissions.editors.includes(person.id);
 
 const hasRequested = (permissions: FormPermissionDto, person: Person) =>
 	permissions.permissionRequests.includes(person.id);

commit 087792b9bf5606586e37ce739c1a494db0f4f9e3

fix form permission check [closes luomus/laji#610]
Bug was introduced by 289696a9ad75269d2a0fbe90f58894ae14c6e047

src/forms/form-permissions/form-permissions.service.ts
@@ -199,6 +199,10 @@ export class FormPermissionsService {
 	}
 
 	async hasEditRightsOf(collectionID: string, person: Person) {
+		const formWithPermissionFeature = await this.findFormWithPermissionFeature(collectionID);
+		if (!formWithPermissionFeature?.options?.restrictAccess) {
+			return true;
+		}
 		const permissions = await this.findByCollectionIDAndPerson(collectionID, person);
 		return !permissions ||  hasEditRightsOf(permissions, person);
 	}