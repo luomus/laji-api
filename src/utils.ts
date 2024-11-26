52fee3e15c1b06f65c80f01eff5583acc6f9c1f5
Olli Raitio <olli.raitio@helsinki.fi>
2024-11-26 11:45:38 +0200

    rewrite promisePipe with async/await

--- src/utils.ts
+++ src/utils.ts
@@ -83,13 +83,13 @@ function promisePipe<T, A, B, C, D, E, F, G, H>(op1: PromiseReducer<T, A>, op2: 
 function promisePipe<T, A, B, C, D, E, F, G, H, I>(op1: PromiseReducer<T, A>, op2: PromiseReducer<A, B>, op3: PromiseReducer<B, C>, op4: PromiseReducer<C, D>, op5: PromiseReducer<D, E>, op6: PromiseReducer<E, F>, op7: PromiseReducer<F, G>, op8: PromiseReducer<G, H>, op9: PromiseReducer<H, I>): PromisePipe<T, I>;
 function promisePipe<T>(...operations: PromiseReducer<any, any>[]): PromisePipe<T, any>;
 function promisePipe<T>(...operations: PromiseReducer<any, any>[]): PromisePipe<T, any> {
-	return (initialValue: T) =>
-		operations.reduce((promise, fn) => promise.then(
-			value => isPromise(fn)
-				? fn(value)
-				: Promise.resolve(fn(value))
-		)
-		, isPromise(initialValue) ? initialValue : Promise.resolve(initialValue));
+	return (initialValue: T) => (async () => {
+		let value = await initialValue;
+		for (const fn of operations) {
+			value = await fn(value);
+		}
+		return value;
+	})();
 }
 
 export { promisePipe, pipe };
