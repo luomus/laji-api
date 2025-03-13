198f837c37d6e3b120ea93e1bae6619ff9ad8f63
Olli Raitio <olli.raitio@helsinki.fi>
2025-03-13 19:05:47 +0200

    add missing global-rest-client

---
+++ src/rest-client/global-rest-client.module.ts
@@ -0,0 +1,21 @@
+import { HttpService } from "@nestjs/axios";
+import { FactoryProvider, Global, Module } from "@nestjs/common";
+import { GLOBAL_CLIENT } from "src/provider-tokens";
+import { RedisCacheService } from "src/redis-cache/redis-cache.service";
+import { RestClientService } from "src/rest-client/rest-client.service";
+
+const GlobalRestClient: FactoryProvider<RestClientService<never>> = {
+	provide: GLOBAL_CLIENT,
+	useFactory: (httpService: HttpService, cache: RedisCacheService) =>
+		new RestClientService(httpService, {
+			name: "global",
+		}, cache),
+	inject: [HttpService, RedisCacheService],
+};
+
+@Global()
+@Module({
+	providers: [GlobalRestClient],
+	exports: [GlobalRestClient]
+})
+export class GlobalRestClientModule {}
