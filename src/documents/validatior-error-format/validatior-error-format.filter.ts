24c5afe7d6c2fb061e6739be33fa111b1aad289a
Olli Raitio <olli.raitio@helsinki.fi>
2025-04-09 15:59:05 +0300

    validation error format filter does't wrap errors in { errors }

--- src/documents/validatior-error-format/validatior-error-format.filter.ts
+++ src/documents/validatior-error-format/validatior-error-format.filter.ts
@@ -1,5 +1,5 @@
 import { ArgumentsHost, Catch } from "@nestjs/common";
-import { ErrorsObj, ValidationException, formatErrorDetails } from "../document-validator/document-validator.utils";
+import { ValidationException, formatErrorDetails } from "../document-validator/document-validator.utils";
 import { Request } from "express";
 import { ValidationErrorFormat } from "../documents.dto";
 import { ErrorSignatureBackwardCompatibilityFilter }
@@ -17,31 +17,6 @@ export class ValidatiorErrorFormatFilter<T> extends ErrorSignatureBackwardCompat
 			(e as any).response.details,
 			validationErrorFormat as ValidationErrorFormat
 		);
-		wrapDetails((e as any).response.details);
 		super.catch(e, host);
 	}
 }
-
-
-// TODO after prod release remove formatting.
-/**
- * Errors are in this format originally:
- *
- * { "/formID": ["Missing required param formID"] }.
- *
- * This function transforms them to be like this:
- *
- * { "/formID": { errors: ["Missing required param formID"] } }.
- *
- * Transformation is done mutably.
- */
-const wrapDetails = (details: ErrorsObj | string[]) => {
-	if (details instanceof Array) {
-		return { errors: details };
-	} else {
-		Object.keys(details).forEach(k => {
-			details[k] = wrapDetails(details[k] as ErrorsObj | string[]);
-		});
-		return details;
-	}
-};
