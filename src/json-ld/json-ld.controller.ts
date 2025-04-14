8399afcdd40560d725a30e7ff1c26bd2ac878510
Olli Raitio <olli.raitio@helsinki.fi>
2025-04-14 13:22:12 +0300

    add missing api tag for /context

--- src/json-ld/json-ld.controller.ts
+++ src/json-ld/json-ld.controller.ts
@@ -2,7 +2,9 @@ import { Get, Param } from "@nestjs/common";
 import { BypassAccessTokenAuth } from "src/access-token/bypass-access-token-auth.decorator";
 import { JsonLdService } from "./json-ld.service";
 import { LajiApiController } from "src/decorators/laji-api-controller.decorator";
+import { ApiTags } from "@nestjs/swagger";
 
+@ApiTags("JSON-LD context")
 @LajiApiController("context")
 export class JsonLdController {
 	constructor(private contextService: JsonLdService) {}
