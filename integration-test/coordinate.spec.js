e186a7bd3a3578ab87626bffcd8d33a0c4876a76
Olli Raitio <olli.raitio@helsinki.fi>
2025-09-12 10:57:02 +0300

    refactor coordinate e2e spec

---
+++ integration-test/coordinate.spec.js
@@ -0,0 +1,11 @@
+var config = require("../config.json");
+var helpers = require("../helpers");
+const { request } = require("chai");
+
+describe("/coordinates", function() {
+	it("returns 404 when no access token specified", async function() {
+		const res = await request(this.server)
+			.get("/coordinates")
+		res.should.have.status(404);
+	});
+});
