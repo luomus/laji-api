54abb05566e36b9d33c349f738608b2d60c7fcac
Olli Raitio <olli.raitio@helsinki.fi>
2025-10-10 13:48:29 +0300

    fix tests by enabling versioning for the test app

--- integration-test/setup.js
+++ integration-test/setup.js
@@ -1,13 +1,19 @@
 const { AppModule } = require("../dist/app.module");
 const { Test } = require("@nestjs/testing");
 const { request } = require("chai");
+const { VersioningType } = require("@nestjs/common");
 
 exports.mochaHooks = {
 	beforeAll: async function() {
 		const module = await Test.createTestingModule({
 			imports: [AppModule],
 		}).compile();
+
 		app = module.createNestApplication();
+		app.enableVersioning({
+			type: VersioningType.HEADER,
+			header: "API-Version",
+		});
 		await app.init();
 		this.server = app.getHttpServer();
 	},
