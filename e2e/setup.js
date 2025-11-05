eaf09b51ed79342acd547c6aa93f9ac1d1fd0644
Olli Raitio <olli.raitio@helsinki.fi>
2025-11-05 11:04:09 +0200

    rm console.log

--- e2e/setup.js
+++ e2e/setup.js
@@ -3,10 +3,8 @@ const { createApp } = require("../dist/create-app");
 exports.mochaHooks = {
 	beforeAll: async function() {
 		const app = await createApp(false);
-		console.log('creating');
 		await app.init();
 		this.server = app.getHttpServer();
-		console.log('created');
 	},
 	afterAll: function () {
 		return this.server.close();
