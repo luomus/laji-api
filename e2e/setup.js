9142a4fa3160c2e9066e6e1ee1f4f91f72296d67
Olli Raitio <olli.raitio@helsinki.fi>
2025-11-05 10:54:54 +0200

    prevent tests from logging (breaks e2e:ci)

--- e2e/setup.js
+++ e2e/setup.js
@@ -2,7 +2,7 @@ const { createApp } = require("../dist/create-app");
 
 exports.mochaHooks = {
 	beforeAll: async function() {
-		const app = await createApp();
+		const app = await createApp(false);
 		console.log('creating');
 		await app.init();
 		this.server = app.getHttpServer();
