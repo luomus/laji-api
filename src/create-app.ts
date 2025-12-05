19484ad529d61152bc5aeac2e47fd01b74709506
Olli Raitio <olli.raitio@helsinki.fi>
2025-12-05 12:08:51 +0200

    bring back v0

--- src/create-app.ts
+++ src/create-app.ts
@@ -50,20 +50,23 @@ export async function createApp(useLogger = true) {
 	const port = configService.get("PORT") || 3004;
 
 	// // ProxyToOldApiService handles redirection to old API. We just check that people don't try to access the new API with API-Version: 1.
-	// app.use("/v0", createProxyMiddleware({
-	// 	target: `http://127.0.0.1:${port}`,
-	// 	pathRewrite: function (path: string) {
-	// 	// pathRewrite: function (path: string, req: Request) {
-	// 		// if (req.headers["api-version"] === "1") {
-	// 		// 	// eslint-disable-next-line max-len
-	// 		// 	throw `Shouldn't use '/v0' in path when using API-Version: 1. Use ${configService.get("MAIL_API_BASE")}${path} instead.`;
-	// 		// }
-	// 		return path;
-	// 	},
-	// 	on: {
-	// 		proxyReq: fixRequestBody
-	// 	},
-	// }));
+	app.use("/v0", createProxyMiddleware({
+		target: `http://127.0.0.1:${port}`,
+		pathRewrite: {
+			"^/v0": "/"
+		},
+		// pathRewrite: function (path: string) {
+		// // pathRewrite: function (path: string, req: Request) {
+		// 	// if (req.headers["api-version"] === "1") {
+		// 	// 	// eslint-disable-next-line max-len
+		// 	// 	throw `Shouldn't use '/v0' in path when using API-Version: 1. Use ${configService.get("MAIL_API_BASE")}${path} instead.`;
+		// 	// }
+		// 	return path;
+		// },
+		on: {
+			proxyReq: fixRequestBody
+		},
+	}));
 
 	// Redirect from the old Swagger explorer to the new.
 	app.use("/explorer", createProxyMiddleware({
