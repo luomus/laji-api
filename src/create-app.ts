a29edf2850e280ad6a902e47099f6a70e15ea7bb
Olli Raitio <olli.raitio@helsinki.fi>
2025-12-10 14:37:35 +0200

    bring back bbw proxies for form permissions & check lists

--- src/create-app.ts
+++ src/create-app.ts
@@ -73,6 +73,16 @@ export async function createApp(useLogger = true) {
 		target: `http://127.0.0.1:${port}`
 	}));
 
+  // Backward compatibity to old API signature of form permissions.
+  app.use("/formPermissions", createProxyMiddleware({
+    target: `http://127.0.0.1:${port}/form-permissions`
+  }));
+
+  // Backward compatibity to old API signature of checklist versions.
+  app.use("/checklistVersions", createProxyMiddleware({
+    target: `http://127.0.0.1:${port}/checklist-versions`
+  }));
+
 	// Backward compatibity to old API signature of person-token.
 	app.use("/person-token", createProxyMiddleware({
 		target: `http://127.0.0.1:${port}/authentication-event`,
