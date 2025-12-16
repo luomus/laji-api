35f46fba0a344fa9f7daf209b9066d09330f6964
Olli Raitio <olli.raitio@helsinki.fi>
2025-12-16 16:28:46 +0200

    migrate area e2e to new token signature

--- e2e/area.spec.js
+++ e2e/area.spec.js
@@ -1,21 +1,20 @@
 var config = require("./config.json");
 var helpers = require("./helpers");
-const { request } = require("chai");
-const { url } = helpers;
-const { access_token } = config;
+const { apiRequest, url } = helpers;
+const { accessToken } = config;
 
 describe("/area", function() {
 	const basePath = "/areas";
 
 	it("returns 401 when no access token specified", async function() {
-		const res = await request(this.server)
+		const res = await apiRequest(this.server)
 			.get(basePath);
 		res.should.have.status(401);
 	});
 
 	it("returns 401 when no access token specified for id", async function() {
 		this.timeout(5000);
-		const res = await request(this.server)
+		const res = await apiRequest(this.server)
 			.get(`${basePath}/ML.206`);
 		res.should.have.status(401);
 	});
@@ -23,8 +22,8 @@ describe("/area", function() {
 	it("returns countries", async function() {
 		this.timeout(5000);
 		var pageSize = 248;
-		const res = await request(this.server)
-			.get(url(basePath, { areaType: "ML.country", pageSize, access_token }));
+		const res = await apiRequest(this.server, { accessToken })
+			.get(url(basePath, { areaType: "ML.country", pageSize }));
 		res.should.have.status(200);
 		helpers.isPagedResult(res.body, pageSize, true);
 		res.body[helpers.params.results].should.have.lengthOf(pageSize);
@@ -36,27 +35,11 @@ describe("/area", function() {
 		}).should.have.lengthOf(pageSize);
 	});
 
-	it("returns countries with deprecated type", async function() {
-		this.timeout(5000);
-		var pageSize = 248;
-		const res = await request(this.server)
-			.get(url(basePath, { type: "country", pageSize, access_token }));
-		res.should.have.status(200);
-		helpers.isPagedResult(res.body, pageSize, true);
-		res.body[helpers.params.results].should.have.lengthOf(pageSize);
-		res.body[helpers.params.results].filter((country) => {
-			return country["id"] === "ML.206"; // a country
-		}).should.have.lengthOf(1);
-		res.body[helpers.params.results].filter((area) => {
-			return area["areaType"] === "ML.country";
-		}).should.have.lengthOf(pageSize);
-	});
-
 	it("returns municipalities", async function() {
 		this.timeout(5000);
 		var pageSize = 2;
-		const res = await request(this.server)
-			.get(url(basePath, { type: "municipality", idIn: "ML.352", pageSize, access_token }));
+		const res = await apiRequest(this.server, { accessToken })
+			.get(url(basePath, { areaType: "ML.municipality", idIn: "ML.352", pageSize }));
 		res.should.have.status(200);
 		helpers.isPagedResult(res.body, pageSize);
 		res.body[helpers.params.results].filter((municipality) => {
@@ -67,8 +50,8 @@ describe("/area", function() {
 
 	it("returns by ids", async function() {
 		const idIn = ["ML.352", "ML.206", "ML.253",  "ML.691"];
-		const res = await request(this.server)
-			.get(url(basePath, { idIn, pageSize: idIn.length, access_token: config.access_token }));
+		const res = await apiRequest(this.server, { accessToken })
+			.get(url(basePath, { idIn, pageSize: idIn.length }));
 		res.should.have.status(200);
 		helpers.isPagedResult(res.body, idIn.length);
 		res.body[helpers.params.results].filter((area) => {
@@ -79,8 +62,8 @@ describe("/area", function() {
 
 	it("returns by id", async function() {
 		this.timeout(5000);
-		const res = await request(this.server)
-			.get(url(`${basePath}/ML.691`, { access_token }));
+		const res = await apiRequest(this.server, { accessToken })
+			.get(`${basePath}/ML.691`);
 		res.should.have.status(200);
 		res.body.should.have.property("id");
 		res.body.should.include({ id: "ML.691" });
