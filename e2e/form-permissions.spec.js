46f4d11ad65f75ad103bd676b8e1cc80e4e1abe7
Olli Raitio <olli.raitio@helsinki.fi>
2025-12-17 10:51:52 +0200

    migrate form permissions e2e to new token signature

--- e2e/form-permissions.spec.js
+++ e2e/form-permissions.spec.js
@@ -1,8 +1,7 @@
 var config = require("./config.json");
 var helpers = require("./helpers");
-const { request } = require("chai");
-const { url } = helpers;
-const { access_token, friend, friend2 } = config;
+const { apiRequest, url } = helpers;
+const { accessToken, friend, friend2 } = config;
 
 const adminToken = friend2.personToken;
 
@@ -12,48 +11,48 @@ describe("/form-permissions", function() {
 
 	it("listing permissions for person works", async function() {
 		this.timeout(30000); // Collections initial load take a while.
-		const res = await request(this.server)
-			.get(url(basePath, { access_token, personToken: friend.personToken }));
+		const res = await apiRequest(this.server, { accessToken, personToken: friend.personToken })
+			.get(basePath);
 		res.should.have.status(200);
 	});
 
 	it("listing permissions for collection with person works", async function() {
 		this.timeout(30000); // Collections initial load take a while.
-		const res = await request(this.server)
-			.get(url(`${basePath}/${collectionID}`, { access_token, personToken: friend.personToken }));
+		const res = await apiRequest(this.server, { accessToken, personToken: friend.personToken })
+			.get(`${basePath}/${collectionID}`);
 		res.should.have.status(200);
 	});
 
 	it("listing permissions for collection without person works", async function() {
-		const res = await request(this.server)
-			.get(url(`${basePath}/${collectionID}`, { access_token }));
+		const res = await apiRequest(this.server, { accessToken })
+			.get(`${basePath}/${collectionID}`);
 		res.should.have.status(200);
 	});
 
 	it("requesting permission works", async function() {
-		const res = await request(this.server)
-			.post(url(`${basePath}/${collectionID}`, { access_token, personToken: friend.personToken }));
+		const res = await apiRequest(this.server, { accessToken, personToken: friend.personToken })
+			.post(`${basePath}/${collectionID}`);
 		res.body.permissionRequests.should.include(friend.id);
 		res.should.have.status(201);
 	});
 
 	it("doesn't allow requesting permission if requested already", async function() {
-		const res = await request(this.server)
-			.post(url(`${basePath}/${collectionID}`, { access_token, personToken: friend.personToken }));
+		const res = await apiRequest(this.server, { accessToken, personToken: friend.personToken })
+			.post(`${basePath}/${collectionID}`);
 		res.should.have.status(406);
 	});
 
 	it("accepting permission works", async function() {
-		const res = await request(this.server)
-			.put(url(`${basePath}/${collectionID}/${friend.id}`, { access_token, personToken: adminToken }));
+		const res = await apiRequest(this.server, { accessToken, personToken: adminToken })
+			.put(`${basePath}/${collectionID}/${friend.id}`);
 		res.body.editors.should.include(friend.id);
 		res.body.permissionRequests.should.not.include(friend.id);
 		res.should.have.status(200);
 	});
 
 	it("granting admin permission works", async function() {
-		const res = await request(this.server)
-			.put(url(`${basePath}/${collectionID}/${friend.id}`, { access_token, personToken: adminToken, type: "admin" }));
+		const res = await apiRequest(this.server, { accessToken, personToken: adminToken })
+			.put(url(`${basePath}/${collectionID}/${friend.id}`, { type: "admin" }));
 		res.body.admins.should.include(friend.id);
 		res.body.editors.should.not.include(friend.id);
 		res.body.permissionRequests.should.not.include(friend.id);
@@ -61,14 +60,14 @@ describe("/form-permissions", function() {
 	});
 
 	it("doesn't allow requesting permission if accepted already", async function() {
-		const res = await request(this.server)
-			.post(url(`${basePath}/${collectionID}`, { access_token, personToken: friend.personToken }));
+		const res = await apiRequest(this.server, { accessToken, personToken: friend.personToken })
+			.post(`${basePath}/${collectionID}`);
 		res.should.have.status(406);
 	});
 
 	it("revoking permission works", async function() {
-		const res = await request(this.server)
-			.delete(url(`${basePath}/${collectionID}/${friend.id}`, { access_token, personToken: adminToken }));
+		const res = await apiRequest(this.server, { accessToken, personToken: adminToken })
+			.delete(`${basePath}/${collectionID}/${friend.id}`);
 		res.body.editors.should.not.include(friend.id);
 		res.body.permissionRequests.should.not.include(friend.id);
 		res.should.have.status(200);
