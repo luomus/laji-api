2a872e8e46c7f4bef6a41744a3b563e585df7f04
Olli Raitio <olli.raitio@helsinki.fi>
2025-12-17 10:45:25 +0200

    migrate checklist-versions e2e to new token signature

--- e2e/checklist-versions.spec.js
+++ e2e/checklist-versions.spec.js
@@ -1,27 +1,28 @@
 const config = require("./config.json");
 const helpers = require("./helpers");
-const { request } = require("chai");
-const { url } = helpers;
-const { access_token } = config;
+const { apiRequest, url } = helpers;
+const { accessToken } = config;
 
 describe("/checklist-versions", function() {
 	const basePath =  "/checklist-versions";
 
 	it("returns 401 when no access token specified", async function() {
-		const res = await request(this.server)
+		const res = await apiRequest(this.server)
 			.get(basePath);
 		res.should.have.status(401);
 	});
 
 	it("returns 401 when no access token specified for id", async function() {
-		const res = await request(this.server).get(`${basePath}/MR.424`);
+		const res = await apiRequest(this.server)
+			.get(`${basePath}/MR.424`);
 		res.should.have.status(401);
 	});
 
 	it("returns page", async function() {
 		this.timeout(5000);
 		const pageSize = 1000;
-		const res = await request(this.server).get(url(basePath, { pageSize, access_token }));
+		const res = await apiRequest(this.server, { accessToken })
+			.get(url(basePath, { pageSize }));
 		res.should.have.status(200);
 		helpers.isPagedResult(res.body, pageSize);
 		res.body[helpers.params.results].filter((checklistVersion) => {
@@ -31,7 +32,8 @@ describe("/checklist-versions", function() {
 
 	it("return item with id", async function() {
 		this.timeout(5000);
-		const res = await request(this.server).get(url(`${basePath}/MR.424`, { access_token }));
+		const res = await apiRequest(this.server, { accessToken })
+			.get(url(`${basePath}/MR.424`));
 		res.should.have.status(200);
 		res.body.should.include({ id: "MR.424" });
 		res.body.should.have.any.keys("@context");
