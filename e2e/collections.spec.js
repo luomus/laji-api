0fe0a99bba5681ceac344000a97a3ac4dd9933b9
Olli Raitio <olli.raitio@helsinki.fi>
2025-12-17 10:48:02 +0200

    migrate collections e2e to new token signature

--- e2e/collections.spec.js
+++ e2e/collections.spec.js
@@ -1,8 +1,7 @@
 const config = require("./config.json");
 const helpers = require("./helpers");
-const { request } = require("chai");
-const { url } = helpers;
-const { access_token } = config;
+const { apiRequest, url } = helpers;
+const { accessToken } = config;
 
 const excludedKeys = ["collectionLocation", "dataLocation", "inMustikka", "editor", "creator"];
 
@@ -13,20 +12,20 @@ describe("/collections", function() {
 	const collectionRootId = "HR.128";
 
 	it("returns 401 when no access token specified", async function() {
-		const res = await request(this.server).get(basePath);
+		const res = await apiRequest(this.server).get(basePath);
 		res.should.have.status(401);
 	});
 
 	it("returns 401 when no access token specified for id", async function() {
-		const res = await request(this.server).get(`${basePath}/${collectionId}`);
+		const res = await apiRequest(this.server).get(`${basePath}/${collectionId}`);
 		res.should.have.status(401);
 	});
 
 	it("return only public collections and has the specified id within", async function() {
 		this.timeout(30000); // Collections initial load take a while.
 		const pageSize = 1000;
-		const res = await request(this.server)
-			.get(url(basePath, { pageSize, access_token }));
+		const res = await apiRequest(this.server, { accessToken })
+			.get(url(basePath, { pageSize }));
 		res.should.have.status(200);
 		helpers.isPagedResult(res.body, pageSize);
 		res.body[helpers.params.results].filter((collection) => {
@@ -39,8 +38,8 @@ describe("/collections", function() {
 	});
 
 	it("return item with id", async function() {
-		const res = await request(this.server)
-			.get(url(`${basePath}/${collectionId}`, { access_token }));
+		const res = await apiRequest(this.server, { accessToken })
+			.get(`${basePath}/${collectionId}`);
 		res.should.have.status(200);
 		res.body.should.not.have.keys(...excludedKeys);
 		res.body.should.have.any.keys("@context");
@@ -48,8 +47,8 @@ describe("/collections", function() {
 	});
 
 	it("returns children item with id", async function() {
-		const res = await request(this.server)
-			.get(url(`${basePath}/${collectionParentId}/children`, { access_token }));
+		const res = await apiRequest(this.server, { accessToken })
+			.get(`${basePath}/${collectionParentId}/children`);
 		res.should.have.status(200);
 		helpers.isPagedResult(res.body);
 		res.body.results.filter((collection) => {
@@ -62,8 +61,8 @@ describe("/collections", function() {
 	});
 
 	it("returns roots", async function() {
-		const res = await request(this.server)
-			.get(url(`${basePath}/roots`, { pageSize: 400, access_token }));
+		const res = await apiRequest(this.server, { accessToken })
+			.get(url(`${basePath}/roots`, { pageSize: 400 }));
 		res.should.have.status(200);
 		helpers.isPagedResult(res.body);
 		res.body.results.filter((collection) => {
