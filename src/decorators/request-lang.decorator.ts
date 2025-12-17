98cb8fd91a6ffa928e01676ae597b21c55818ed4
Olli Raitio <olli.raitio@helsinki.fi>
2025-12-17 12:17:10 +0200

    fix request-lang decorator (regression from ad0a7ef9)

--- src/decorators/request-lang.decorator.ts
+++ src/decorators/request-lang.decorator.ts
@@ -2,7 +2,7 @@ import { createParamDecorator, ExecutionContext } from "@nestjs/common";
 import { getLang } from "src/interceptors/translator.interceptor";
 
 export const RequestLang = createParamDecorator((
-	{ allowMulti = true }: { allowMulti?: boolean },
+	{ allowMulti = true }: { allowMulti?: boolean } = {},
 	ctx: ExecutionContext
 ) => 
 	getLang(ctx.switchToHttp().getRequest(), allowMulti)

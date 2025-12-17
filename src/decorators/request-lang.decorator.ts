luomus/laji-api

commit ad0a7ef9dbba224732fce99ac46b9e3b5f014e96

migrate information e2e to new token signature

src/decorators/request-lang.decorator.ts
@@ -1,6 +1,9 @@
 import { createParamDecorator, ExecutionContext } from "@nestjs/common";
 import { getLang } from "src/interceptors/translator.interceptor";
 
-export const RequestLang = createParamDecorator((_, ctx: ExecutionContext) => {
-	return getLang(ctx.switchToHttp().getRequest());
-});
+export const RequestLang = createParamDecorator((
+	{ allowMulti = true }: { allowMulti?: boolean },
+	ctx: ExecutionContext
+) => 
+	getLang(ctx.switchToHttp().getRequest(), allowMulti)
+);

e2e/information.spec.js
@@ -1,28 +1,27 @@
 var config = require("./config.json");
 var helpers = require("./helpers");
-const { request } = require("chai");
-const { url } = helpers;
-const { access_token } = config;
+const { apiRequest } = helpers;
+const { accessToken } = config;
 
 describe("/information", function() {
 	var basePath =  "/information";
 	const id =  "5660";
 
 	it("returns 401 when no access token specified", async function() {
-		const res = await request(this.server)
+		const res = await apiRequest(this.server)
 			.get(basePath);
 		res.should.have.status(401);
 	});
 
 	it("returns 401 when no access token specified for id", async function() {
-		const res = await request(this.server)
+		const res = await apiRequest(this.server)
 			.get(`${basePath}/${id}`);
 		res.should.have.status(401);
 	});
 
 	it("returns by id parsed", async function() {
-		const res = await request(this.server)
-			.get(url(`${basePath}/${id}`, { access_token }));
+		const res = await apiRequest(this.server, { accessToken })
+			.get(`${basePath}/${id}`);
 		res.should.have.status(200);
 		res.body.id.should.be.a("string");
 		res.body.content.should.be.a("string");
@@ -38,8 +37,8 @@ describe("/information", function() {
 	});
 
 	it("returns all parsed", async function() {
-		const res = await request(this.server)
-			.get(url(basePath, { access_token }));
+		const res = await apiRequest(this.server, { accessToken })
+			.get(basePath);
 		res.should.have.status(200);
 		res.body.id.should.be.a("string");
 		res.body.content.should.be.a("string");
@@ -52,15 +51,15 @@ describe("/information", function() {
 	});
 
 	it("all respects lang param", async function() {
-		const res = await request(this.server)
-			.get(url(basePath, { access_token, lang: "fi" }));
+		const res = await apiRequest(this.server, { accessToken, lang: "fi" })
+			.get(basePath);
 		res.should.have.status(200);
 		res.body.title.should.equal("FI");
 	});
 
 	it("returns index", async function() {
-		const res = await request(this.server)
-			.get(url(`${basePath}/index`, { access_token }));
+		const res = await apiRequest(this.server, { accessToken })
+			.get(`${basePath}/index`);
 		res.should.have.status(200);
 		res.body.should.have.property("page");
 		res.body.should.have.property("children");
@@ -68,8 +67,8 @@ describe("/information", function() {
 	});
 
 	it("index respects lang param", async function() {
-		const res = await request(this.server)
-			.get(url(`${basePath}/index`, { access_token, lang: "fi" }));
+		const res = await apiRequest(this.server, { accessToken, lang: "fi" })
+			.get(`${basePath}/index`);
 		res.should.have.status(200);
 		res.body.page.title.should.equal("FI");
 	});

e2e/named-places.spec.js
@@ -1,6 +1,6 @@
 const config = require("./config.json");
 const helpers = require("./helpers");
-const { request, expect } = require("chai");
+const { expect } = require("chai");
 const { dateToISODate } = require("../dist/utils");
 const { accessToken, personToken, person, friend, friend2 } = config;
 const { apiRequest, url } = helpers;

src/information/information.controller.ts
@@ -1,8 +1,9 @@
-import { Get, Param, Query } from "@nestjs/common";
+import { Get, Param } from "@nestjs/common";
 import { InformationService } from "./information.service";
-import { GetAllInformationDto } from "./information.dto";
 import { LajiApiController } from "src/decorators/laji-api-controller.decorator";
 import { ApiTags } from "@nestjs/swagger";
+import { RequestLang } from "src/decorators/request-lang.decorator";
+import { Lang } from "src/common.dto";
 
 @ApiTags("Informations")
 @LajiApiController("information")
@@ -10,12 +11,10 @@ export class InformationController {
 
 	constructor(private informationService: InformationService) {}
 
-	/** Returns id of the index page of some language */
+	/** Returns id of the index page. Allowed languages are 'fi', 'sv', 'en'.  */
 	@Get("index")
-	getIndex(@Query() { lang }: GetAllInformationDto) {
-		// '!' is valid here, because DTO classes must have '?' modifier for properties with defaults, making the
-		// typings bit awkward.
-		return this.informationService.getIndex(lang!);
+	getIndex(@RequestLang({ allowMulti: false }) lang: Omit<Lang, "multi">) {
+		return this.informationService.getIndex(lang);
 	}
 
 	/** Get information page by id */
@@ -24,12 +23,10 @@ export class InformationController {
 		return this.informationService.get(id);
 	}
 
-	/** Get information page contents */
+	/** Get information page contents. Allowed languages are 'fi', 'sv', 'en'. */
 	@Get()
-	getAll(@Query() { lang }: GetAllInformationDto) {
-		// '!' is valid here, because DTO classes must have '?' modifier for properties with defaults, making the
-		// typings bit awkward.
-		return this.informationService.getAll(lang!);
+	getAll(@RequestLang({ allowMulti: false }) lang: Omit<Lang, "multi">) {
+		return this.informationService.getAll(lang);
 	}
 
 }

src/information/information.dto.ts
@@ -1,13 +1,3 @@
-export enum InformationLang {
-	fi = "fi",
-	sv = "sv",
-	en = "en",
-}
-
-export class GetAllInformationDto {
-	lang?: InformationLang = InformationLang.en;
-}
-
 class InformationChild {
 	title: string;
 	id: string;

src/information/information.service.ts
@@ -1,15 +1,16 @@
 import { HttpException, Inject, Injectable } from "@nestjs/common";
 import { INFORMATION_CLIENT } from "src/provider-tokens";
 import { RestClientService } from "src/rest-client/rest-client.service";
-import { Information, InformationLang, RemoteInformation } from "./information.dto";
+import { Information, RemoteInformation } from "./information.dto";
 import { serializeInto } from "src/serialization/serialization.utils";
+import { Lang } from "src/common.dto";
 
 @Injectable()
 export class InformationService {
 
 	constructor(@Inject(INFORMATION_CLIENT) private informationClient: RestClientService<RemoteInformation>) {}
 
-	async getAll(lang: InformationLang) {
+	async getAll(lang: Omit<Lang, "multi">) {
 		return parseInformation(await this.informationClient.get(undefined, { params: { locale: lang } }));
 	}
 
@@ -17,7 +18,7 @@ export class InformationService {
 		return parseInformation(await this.informationClient.get(id));
 	}
 
-	getIndex(lang: InformationLang) {
+	getIndex(lang: Omit<Lang, "multi">) {
 		return this.informationClient.get(undefined, { params: { locale: lang } });
 	}
 }

src/interceptors/translator.interceptor.ts
@@ -65,14 +65,14 @@ const takeSample = (result: any) => {
 	return result;
 };
 
-export const getLang = (request: Request): Lang => {
+export const getLang = (request: Request, allowMulti = true): Lang => {
 	const { query, headers } = request;
 	if (query.lang) {
 		return parseLangFromQuery(query.lang as string);
 	}
 	const acceptLanguage = headers["accept-language"];
 	if (acceptLanguage) {
-		return parseLangFromHeader(acceptLanguage);
+		return parseLangFromHeader(acceptLanguage, allowMulti);
 	}
 	return Lang.en;
 };
@@ -85,13 +85,13 @@ const parseLangFromQuery = (lang: string) => {
 	return validLang;
 };
 
-const parseLangFromHeader = (acceptLanguage: string): Lang => {
+const parseLangFromHeader = (acceptLanguage: string, allowMulti = true): Lang => {
 	const lang = firstFromNonEmptyArr(acceptLanguage.split(",")).toLowerCase();
 	if (lang.startsWith("fi")) {
 		return Lang.fi;
 	} else if (lang.startsWith("sv")) {
 		return Lang.sv;
-	} else if (lang.startsWith("mul")) {
+	} else if (allowMulti && lang.startsWith("mul")) {
 		return Lang.multi;
 	}
 	return Lang.en;

commit 98cb8fd91a6ffa928e01676ae597b21c55818ed4

fix request-lang decorator (regression from ad0a7ef9)

src/decorators/request-lang.decorator.ts
@@ -2,7 +2,7 @@ import { createParamDecorator, ExecutionContext } from "@nestjs/common";
 import { getLang } from "src/interceptors/translator.interceptor";
 
 export const RequestLang = createParamDecorator((
-	{ allowMulti = true }: { allowMulti?: boolean },
+	{ allowMulti = true }: { allowMulti?: boolean } = {},
 	ctx: ExecutionContext
 ) => 
 	getLang(ctx.switchToHttp().getRequest(), allowMulti)