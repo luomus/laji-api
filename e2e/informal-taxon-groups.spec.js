26b292d6b870c1741de28490386e944235b80b27
Olli Raitio <olli.raitio@helsinki.fi>
2025-12-17 10:55:01 +0200

    migrate informal-taxon-groups e2e to new token signature

--- e2e/informal-taxon-groups.spec.js
+++ e2e/informal-taxon-groups.spec.js
@@ -1,21 +1,20 @@
 const config = require("./config.json");
 const helpers = require("./helpers");
-const { request } = require("chai");
-const { url } = helpers;
-const { access_token } = config;
+const { apiRequest, url } = helpers;
+const { accessToken } = config;
 
 describe("/informal-taxon-groups", function() {
 	const basePath =  "/informal-taxon-groups";
 
 	it("returns 401 when no access token specified", async function() {
-		const res = await request(this.server)
+		const res = await apiRequest(this.server)
 			.get(basePath);
 		res.should.have.status(401);
 	});
 
 	it("returns 401 when no access token specified for id", async function() {
 		this.timeout(5000);
-		const res = await request(this.server)
+		const res = await apiRequest(this.server)
 			.get(url(`${basePath}/MVL.33`));
 		res.should.have.status(401);
 	});
@@ -23,8 +22,8 @@ describe("/informal-taxon-groups", function() {
 	it("returns informal taxon groups", async function() {
 		this.timeout(5000);
 		var pageSize = 100;
-		const res = await request(this.server)
-			.get(url(basePath, { pageSize, access_token }));
+		const res = await apiRequest(this.server, { accessToken })
+			.get(url(basePath, { pageSize }));
 		res.should.have.status(200);
 		res.body.results.should.have.lengthOf(pageSize);
 		res.body.results.filter(group => {
@@ -36,8 +35,8 @@ describe("/informal-taxon-groups", function() {
 		var ids = [
 			"MVL.33","MVL.232"
 		];
-		const res = await request(this.server)
-			.get(url(basePath, { idIn: ids.join(","), pageSize: ids.length, access_token }));
+		const res = await apiRequest(this.server, { accessToken })
+			.get(url(basePath, { idIn: ids.join(","), pageSize: ids.length }));
 		res.should.have.status(200);
 		helpers.isPagedResult(res.body, ids.length);
 		res.body.results.filter((group) => {
@@ -49,8 +48,8 @@ describe("/informal-taxon-groups", function() {
 	it("returns by id", async function() {
 		this.timeout(5000);
 		var testId = "MVL.33";
-		const res = await request(this.server)
-			.get(url(`${basePath}/${testId}`, { access_token }));
+		const res = await apiRequest(this.server, { accessToken })
+			.get(`${basePath}/${testId}`);
 		res.should.have.status(200);
 		res.body.should.have.property("id");
 		res.body.should.include({ id: testId });
@@ -58,8 +57,8 @@ describe("/informal-taxon-groups", function() {
 
 	it("returns parent by id", async function() {
 		this.timeout(5000);
-		const res = await request(this.server)
-			.get(url(`${basePath}/MVL.1241/parent`, { access_token }));
+		const res = await apiRequest(this.server, { accessToken })
+			.get(`${basePath}/MVL.1241/parent`);
 		res.should.have.status(200);
 		res.body.should.have.property("id");
 		res.body.should.include({ id: "MVL.1" });
