68cde1ae2e405a024ca020fcc04c0bb7a64c7d91
Olli Raitio <olli.raitio@helsinki.fi>
2026-02-04 12:42:19 +0200

    rm unused vars

--- src/swagger/swagger.service.ts
+++ src/swagger/swagger.service.ts
@@ -34,8 +34,6 @@ export class SwaggerService {
 	private logger = new Logger(SwaggerService.name);
 	private rawDocument: OpenAPIObject;
 
-	storeSwaggerDoc?: OpenAPIObject;
-	lajiBackendSwaggerDoc?: OpenAPIObject;
 	remoteSwaggers: Record<string, {
 		instance: {
 			fetchSwagger: FetchSwagger,
@@ -61,8 +59,8 @@ export class SwaggerService {
 
 	@Interval(CACHE_30_MIN)
 	async warmup() {
-		this.storeSwaggerDoc = await this.getStoreSwaggerDoc();
-		this.lajiBackendSwaggerDoc = await this.getLajiBackendSwaggerDoc();
+		await this.getStoreSwaggerDoc();
+		await this.getLajiBackendSwaggerDoc();
 		await Promise.all(instancesWithRemoteSwagger.map(this.fetchRemoteSwagger));
 	}
 
