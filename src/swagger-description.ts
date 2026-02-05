4201f919f0265a72d76cf9fd4d3551ddfac4b090
esko-piirainen <esko.piirainen@helsinki.fi>
2026-02-05 08:54:35 +0200

    api frontpage documentation

--- src/swagger-description.ts
+++ src/swagger-description.ts
@@ -1,11 +1,29 @@
 /* eslint-disable max-len */
 export const swaggerDescription =
 `
-Access token is needed to use this API. To get a token, send a POST request with your email address to /api-users (under section "API User") endpoint and one will be sent to your email. Include the token to each request as a Bearer token (http header "Authorization: Bearer \\<ACCESS TOKEN\\>")
+## Authentication
 
-Access token, person token and language can be globally configured using the "Authorize" button.
+This API requires an **Access Token**. To obtain a token:
+1. Send a \`POST\` request with your email address to the \`/api-user\` endpoint (see **API User** section).
+2. An access token will be sent to your email.
 
-You can find more documentation [here](https://info.laji.fi/en/frontpage/api/api-laji-fi/).
+Include the token in all requests using the HTTP header: \`Authorization: Bearer <ACCESS_TOKEN>\`
 
-If you have any questions you can contact us at helpdesk@laji.fi.
+Here on the API frontpage, you can configure the **Access Token**, **Person-Token**, and **Accept-Language** globally using the **Authorize** button above.
+
+---
+
+## Documentation & Resources
+
+- **Additional API documentation:**  
+  https://info.laji.fi/en/frontpage/api/api-laji-fi/
+
+- **OpenAPI / Swagger 3.0 specification (machine-readable):**  
+  https://api.laji.fi/openapi-json
+
+---
+
+## Support
+
+If you have any questions, contact us at **helpdesk@laji.fi**.
 `;
