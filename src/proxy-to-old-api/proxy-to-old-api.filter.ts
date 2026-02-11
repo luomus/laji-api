0b741f60b9f8e280ea26393746e1705dc3f892aa
Olli Raitio <olli.raitio@helsinki.fi>
2026-02-11 13:31:04 +0200

    fix regression from 331e9c

--- src/proxy-to-old-api/proxy-to-old-api.filter.ts
+++ src/proxy-to-old-api/proxy-to-old-api.filter.ts
@@ -1,16 +1,20 @@
-import { ArgumentsHost, Catch, ExceptionFilter, NotFoundException } from "@nestjs/common";
+import { ArgumentsHost, Catch, NotFoundException } from "@nestjs/common";
 import { NextFunction, Request, Response } from "express";
 import { ProxyToOldApiService } from "./proxy-to-old-api.service";
+import { BaseExceptionFilter } from "@nestjs/core";
 
 @Catch(NotFoundException)
-export class ProxyToOldApiFilter implements ExceptionFilter {
-	constructor(private readonly proxyToOldApiService: ProxyToOldApiService) {}
+export class ProxyToOldApiFilter extends BaseExceptionFilter {
+	constructor(private readonly proxyToOldApiService: ProxyToOldApiService) {
+		super();
+	}
 
 	async catch(exception: NotFoundException, host: ArgumentsHost) {
 		const request = host.switchToHttp().getRequest<Request>();
 
 		if (request.headers["api-version"] === "1") {
-			throw exception;
+			super.catch(exception, host);
+			return;
 		}
 
 		const response = host.switchToHttp().getResponse<Response>();
