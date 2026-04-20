luomus/laji-api

commit 03cf40ea73772b5ee4379b86292e373f9194fb7b

person token / access token system id cross check

src/interceptors/person-token.interceptor.ts
@@ -1,8 +1,10 @@
 import { CallHandler, ExecutionContext, HttpException, Injectable, NestInterceptor } from "@nestjs/common";
 import { Observable } from "rxjs";
 import { PersonsService } from "src/persons/persons.service";
-import { Request } from "express";
+import { Request } from "src/request";
 import { Reflector } from "@nestjs/core";
+import { AuthenticationEventService } from "src/authentication-event/authentication-event.service";
+import { ErrorCodeException } from "src/utils";
 
 /**
  * Extracts a person from the request's person token, implanting the person object into the request object so it can be
@@ -11,7 +13,11 @@ import { Reflector } from "@nestjs/core";
 @Injectable()
 export class PersonTokenInterceptor implements NestInterceptor {
 
-	constructor(private personsService: PersonsService, private reflector: Reflector) {}
+	constructor(
+		private personsService: PersonsService, 
+		private reflector: Reflector,
+		private authenticationEventService: AuthenticationEventService
+	) {}
 
 	async intercept(context: ExecutionContext, next: CallHandler): Promise<Observable<any>> {
 		const request = context.switchToHttp().getRequest<Request>();
@@ -22,6 +28,12 @@ export class PersonTokenInterceptor implements NestInterceptor {
 
 		const personToken = getPersonTokenFromRequest(request);
 		if (personToken && typeof personToken === "string") {
+			const authEvent = await this.authenticationEventService.getInfo(personToken);
+			if (request.apiUser?.systemID !== authEvent.target) {
+				const error = new ErrorCodeException("ACCESS_TOKEN_PERSON_TOKEN_MISMATCH", 401);
+				error.message = "The person token is for a different system than which the access token is for";
+				throw error;
+			}
 			(request as any).person = await this.personsService.getByToken(personToken);
 		}
 		return next.handle();

src/authentication-event/authentication-event.controller.ts
@@ -1,6 +1,6 @@
 import { Delete, Get, Param, Version, Headers, HttpCode } from "@nestjs/common";
 import { ApiExcludeEndpoint, ApiTags } from "@nestjs/swagger";
-import { PersonTokenService } from "./authentication-event.service";
+import { AuthenticationEventService } from "./authentication-event.service";
 import { LajiApiController } from "src/decorators/laji-api-controller.decorator";
 import { BypassPersonTokenInterceptor } from "./bypass-person-token-interceptor.decorator";
 
@@ -8,7 +8,7 @@ import { BypassPersonTokenInterceptor } from "./bypass-person-token-interceptor.
 @ApiTags("Authentication event")
 export class PersonTokenController {
 
-	constructor(private personTokenService: PersonTokenService) {}
+	constructor(private personTokenService: AuthenticationEventService) {}
 
 	@ApiExcludeEndpoint()
 	@Get(":personToken")

src/authentication-event/authentication-event.module.ts
@@ -1,12 +1,12 @@
 import { Module } from "@nestjs/common";
-import { PersonTokenService } from "./authentication-event.service";
+import { AuthenticationEventService } from "./authentication-event.service";
 import { PersonTokenController } from "./authentication-event.controller";
 import { LajiAuthClientModule } from "src/laji-auth-client/laji-auth-client.module";
 
 @Module({
 	imports: [LajiAuthClientModule],
-	providers: [PersonTokenService],
-	exports: [PersonTokenService],
+	providers: [AuthenticationEventService],
+	exports: [AuthenticationEventService],
 	controllers: [PersonTokenController]
 })
 export class PersonTokenModule {}

src/authentication-event/authentication-event.service.ts
@@ -5,7 +5,7 @@ import { MS_1_SEC } from "src/utils";
 import { LAJI_AUTH_CLIENT } from "src/provider-tokens";
 
 @Injectable()
-export class PersonTokenService {
+export class AuthenticationEventService {
 
 	constructor(
 		@Inject(LAJI_AUTH_CLIENT) private lajiAuthClient: RestClientService<LajiAuthPersonGet>

src/persons/persons.service.ts
@@ -1,6 +1,6 @@
 import { HttpException, Injectable } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
-import { PersonTokenService } from "src/authentication-event/authentication-event.service";
+import { AuthenticationEventService } from "src/authentication-event/authentication-event.service";
 import { TriplestoreService } from "src/triplestore/triplestore.service";
 import { decoratePerson, Person, Role } from "./person.dto";
 import { serializeInto } from "src/serialization/serialization.utils";
@@ -11,7 +11,7 @@ import { PersonTokenInfo } from "src/authentication-event/authentication-event.d
 export class PersonsService {
 	constructor(
 		private configService: ConfigService,
-		private personTokenService: PersonTokenService,
+		private personTokenService: AuthenticationEventService,
 		private triplestoreService: TriplestoreService
 	) {}
 

commit 49445ba45bdbf7af773c7c6aaa50c0687f695a49

fix importer token (regression from 03cf40ea)

src/interceptors/person-token.interceptor.ts
@@ -5,6 +5,7 @@ import { Request } from "src/request";
 import { Reflector } from "@nestjs/core";
 import { AuthenticationEventService } from "src/authentication-event/authentication-event.service";
 import { ErrorCodeException } from "src/utils";
+import { ConfigService } from "@nestjs/config";
 
 /**
  * Extracts a person from the request's person token, implanting the person object into the request object so it can be
@@ -16,7 +17,8 @@ export class PersonTokenInterceptor implements NestInterceptor {
 	constructor(
 		private personsService: PersonsService, 
 		private reflector: Reflector,
-		private authenticationEventService: AuthenticationEventService
+		private authenticationEventService: AuthenticationEventService,
+		private config: ConfigService
 	) {}
 
 	async intercept(context: ExecutionContext, next: CallHandler): Promise<Observable<any>> {
@@ -28,11 +30,13 @@ export class PersonTokenInterceptor implements NestInterceptor {
 
 		const personToken = getPersonTokenFromRequest(request);
 		if (personToken && typeof personToken === "string") {
-			const authEvent = await this.authenticationEventService.getInfo(personToken);
-			if (request.apiUser?.systemID !== authEvent.target) {
-				const error = new ErrorCodeException("ACCESS_TOKEN_PERSON_TOKEN_MISMATCH", 401);
-				error.message = "The person token is for a different system than which the access token is for";
-				throw error;
+			if (personToken !== this.config.get("IMPORTER_TOKEN")) {
+				const authEvent = await this.authenticationEventService.getInfo(personToken);
+				if (request.apiUser?.systemID !== authEvent.target) {
+					const error = new ErrorCodeException("ACCESS_TOKEN_PERSON_TOKEN_MISMATCH", 401);
+					error.message = "The person token is for a different system than which the access token is for";
+					throw error;
+				}
 			}
 			(request as any).person = await this.personsService.getByToken(personToken);
 		}