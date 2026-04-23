77494373d8d5dd95f7ed308da6d0728419d9ade3
Olli Raitio <olli.raitio@helsinki.fi>
2026-04-23 16:41:39 +0300

    update DOCUMENT_VALIDATION_MA_CODES_UNFAMILIAR translations
    fixes luomus/laji#830

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
 import { ConfigService } from "@nestjs/config";
 import { createApp } from "./create-app";
 
-async function bootstrap() {
+void (async () => {
 	const app = await createApp();
 	const port = app.get(ConfigService).get("PORT") || 3004;
 	await app.listen(port, "0.0.0.0");
-}
-void bootstrap();
+})();
