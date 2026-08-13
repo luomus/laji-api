57fdb7729d6b173cc590240a98c914c916e4e6a7
Olli Raitio <olli.raitio@helsinki.fi>
2026-08-13 16:40:29 +0300

    graphql schema uses strings instead of enums

--- mesh.config.ts
+++ mesh.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "@graphql-mesh/compose-cli";
 import { loadOpenAPISubgraph } from "@omnigraph/openapi";
 import "dotenv";
- 
+import { GraphQLSchema, GraphQLString, getNamedType, isEnumType } from "graphql";
+import { mapSchema, MapperKind } from "@graphql-tools/utils";
+
 const PORT = process.env.PORT || "3005";
 
 export const composeConfig = defineConfig({
@@ -13,7 +15,36 @@ export const composeConfig = defineConfig({
 					"Accept": "application/json",
 				},
 				ignoreErrorResponses: true,
-			})
+			}),
+			transforms: [
+				// We do this because GraphQL enums don't allow dot character in enums. The generated schema would have the
+				// enums with dots replaced with lower dash. Our enums have dots (like `MY.collectionQuality`), and we want to
+				// keep them to keep ids stable.
+				replaceEnumsWithStrings
+			]
 		}
-	]
+	],
 });
+
+export function replaceEnumsWithStrings(schema: GraphQLSchema) {
+	return mapSchema(schema, {
+		[MapperKind.OBJECT_FIELD]: (fieldConfig) => {
+			const namedType = getNamedType(fieldConfig.type);
+
+			if (!isEnumType(namedType)) {
+				return fieldConfig;
+			}
+
+			fieldConfig.type = GraphQLString;
+
+			const source =
+				(fieldConfig.extensions as any)?.directives?.source;
+
+			if (source?.type) {
+				source.type = "String";
+			}
+
+			return fieldConfig;
+		},
+	});
+}
